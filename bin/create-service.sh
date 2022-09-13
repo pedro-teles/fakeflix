e0e497fdec610f46cedd84f79a0bc77322b90d44
Pedro Teles <pedroateles@protonmail.com>
2022-09-13 20:48:49 -0300

    fix: add command to remove .git folder in the script

--- bin/create-service.sh
+++ bin/create-service.sh
@@ -10,3 +10,5 @@ mv $1/test/diplomat_architecture_template $1/test/$1
 
 find $1 -type f -exec sed -i "s/diplomat-architecture-template/$1/g" {} \;
 find $1 -type f -exec sed -i "s/template/$1/g" {} \;
+
+rm -rf $1/.git
