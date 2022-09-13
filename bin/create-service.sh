7850166a0ece1f6d4453c75951b0b2bb4a3676f6
Pedro Teles <pedroateles@protonmail.com>
2022-09-13 20:42:41 -0300

    feat: add script to create a service Add a script that will clone the service template and update the project name

---
+++ bin/create-service.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+BASEDIR=$(dirname "$0")
+
+cd $BASEDIR/../services
+
+git clone git@github.com:pedro-teles/diplomat-architecture-template.git $1
+
+mv $1/src/diplomat_architecture_template $1/src/$1
+mv $1/test/diplomat_architecture_template $1/test/$1
+
+find $1 -type f -exec sed -i "s/diplomat-architecture-template/$1/g" {} \;
+find $1 -type f -exec sed -i "s/template/$1/g" {} \;
