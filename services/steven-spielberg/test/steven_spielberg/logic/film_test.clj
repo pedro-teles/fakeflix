38183aa40c83932b2f2d9d28af12f55ab3a8769f
Pedro Teles <pedroateles@protonmail.com>
2022-09-20 20:33:42 -0300

    fix: exception on id generation function Add a function to create a SHA-256 of the id before turning it into bytes. Some special characters were throwing some expcetions when generating their bytes

--- services/steven-spielberg/test/steven_spielberg/logic/film_test.clj
+++ services/steven-spielberg/test/steven_spielberg/logic/film_test.clj
@@ -33,3 +33,11 @@
                  :film/release-date  "2022-08-31"
                  :film/vote-average  5.0}
                 (logic.film/associate-id (assoc fixtures.film/model :film/title "Another Unit Test"))))))
+
+(deftest sha256-test
+  (testing "Should generate the correct SHA-256 string from the given value"
+    (is (= "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92"
+           (logic.film/sha256 "123456")))
+
+    (is (= "cf80cd8aed482d5d1527d7dc72fceff84e6326592848447d2dc0b0e87dfc9a90"
+           (logic.film/sha256 "testing")))))
